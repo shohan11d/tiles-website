57dfa767a0606713705e957d7270bca75d4dd6dc
shohan11d <shohan11d@gmail.com>
2026-05-06 01:40:18 +0600

    dns added

--- src/proxy.js
+++ src/proxy.js
@@ -1,3 +1,7 @@
+
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import { NextResponse } from "next/server";
 import { authClient } from "./lib/auth-client";
 import { auth } from "./lib/auth";
